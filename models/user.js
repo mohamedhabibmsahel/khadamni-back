1cc8b49dc1b037a5c4009a959b47679c3dad317c
m'sahel mohamed habib <48933499+mohamedhabibmsahel@users.noreply.github.com>
2021-11-20 23:38:00 +0100

    init project

---
+++ models/user.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose')
+
+const userSchema = new mongoose.Schema({
+    nom: {
+        type: String,
+        required: true,
+    },
+    prenom: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required:true
+    },
+    numt:{
+        type: String,
+        required: true
+    },
+    photoProfil:{
+        type: String,
+        required: false
+    },
+    job:{
+        type: String,
+        required: false
+    }
+    
+})
+
+module.exports = mongoose.model('user',userSchema)
---
+++ server.js
@@ -0,0 +1,23 @@
+require("dotenv").config()
+
+const express = require ('express' )
+const app = express()
+const mongoose = require ("mongoose")
+const path = require('path')
+
+mongoose.connect(process.env.DB_URL, {useNewUrlParser: true})
+
+const db = mongoose.connection
+
+db.on ("error", (error) => console.error(error))
+db.once('open',() => console.log("Connected to DB"))
+
+app.use(express.json())
+
+app.use('/upload',express.static(path.join(__dirname,'upload')))
+
+const userRoute = require('./routes/user')
+app.use('/user',userRoute)
+
+
+app.listen(3000, () => console.log("Server Started"))
